5b32b13d0de2b60e55298e3dca4a6dc96f33b7e0
Rodrigo <roi31@hotmail.com>
2020-01-19 19:36:25 +0100

    Finalizacion ultimos ejercicios clase

---
+++ js-basic.js
@@ -0,0 +1,66 @@
+// EJERCICIO 1
+// console.log('hola mundo');
+
+// EJERCICIO 2
+// alert('Me llamo Rodrigo!');
+
+// EJERCICIO 3
+// var nombre = 'Rodrigo';
+// var apellido = "Castro";
+// console.log(nombre +' '+  apellido);
+
+// EJERCICIO 4
+// var numA = 58;
+// var numB = 32;
+// var result = numA + numB;
+// console.log('la suma entre ' + numA + ' y '+ numB + ' es ' + result );
+
+// EJERCICIO 5
+// var nota = 2.5;
+
+// if(nota >= 5){
+//   alert('Has aprobado con un ' + nota);
+// }else{
+//   alert('Creo que suspendiste con un ' + nota);
+// }
+
+// EJERCICIO 6
+// var color = 'verde';
+// var sentence = 'Tengo un coche de color ';
+
+// console.log(sentence + color);
+
+// color = color.replace('verde','azul');
+// sentence = sentence.replace(new RegExp('o', 'g'),'u');
+// console.log(sentence + color);
+
+// // EJERCICIO 7
+// var objetos = ["mesa", "silla", "odenador","libreta"];
+
+
+// for(i = 0; i < objetos.length; i++){
+
+// console.log("El objeto " + objetos[i] + " esta en la posicion " + i);
+
+// EJERCICIO 8
+// var resultado = calculadora('division', 1900, 55) 
+
+// function calculadora(operador, valor1, valor2){
+// 	if(operador == 'resta'){
+//   	 return (valor1 - valor2);
+     
+//   }else if(operador == 'suma'){
+//   	return(valor1 + valor2);
+  
+//   }else if(operador == 'multiplicacion'){
+//   	  	return(valor1 * valor2);
+        
+//   }else if(operador == 'division'){
+//   			return(valor1 / valor2);
+//   }else{
+//   	return "Usa un operador útil";
+//   }
+
+// }
+
+// console.log(resultado);
